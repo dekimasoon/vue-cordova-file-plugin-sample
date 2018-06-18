c1dd23b026980e20a1bfa8f012d12d96a7353cad
dekimasoon <dekimasoon@gmail.com>
2018-06-18 13:09:27 +0900

    invoke vue-cli-plugin-cordova

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list dense>
+        <v-list-tile @click="route('/')">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="route('/about')">
+          <v-list-tile-action>
+            <v-icon>info_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>About</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'app',
+export default Vue.extend({
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  methods: {
+    route(path) {
+      alert('no router! ' + path);
+    },
+  },
   components: {
     HelloWorld
-  }
-}
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #fafafa;
+  padding-top: constant(safe-area-inset-top);
+  padding-top: env(safe-area-inset-top);
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom);
+  padding-left: constant(safe-area-inset-left);
+  padding-left: env(safe-area-inset-left);
+  padding-right: constant(safe-area-inset-right);
+  padding-right: env(safe-area-inset-right);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import 'typeface-roboto';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'vuetify/dist/vuetify.css';
+import 'babel-polyfill';
 import Vue from 'vue'
 import App from './App.vue'
+import cordovaLoader from './cordovaLoader';
+import vuetify from 'vuetify';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(vuetify);
+
+cordovaLoader(() => {
+  new Vue({
+    render: h => h(App)
+  }).$mount('#app')
+});
